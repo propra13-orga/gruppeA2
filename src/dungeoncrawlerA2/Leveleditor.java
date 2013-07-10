f1653818a11d8a6076ad86e9dc79052f4190cc07
Christoph Danell <Christoph.Danell@uni-duesseldorf.de>
2013-07-10 18:21:41 +0200

    Leveleditor erweitert - Räume betrachten und  wechseln möglich

--- src/dungeoncrawlerA2/Leveleditor.java
+++ src/dungeoncrawlerA2/Leveleditor.java
@@ -1,11 +1,16 @@
 package dungeoncrawlerA2;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import javax.swing.JButton;
@@ -14,6 +19,9 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 public class Leveleditor extends JFrame implements ActionListener{
+	
+	// Editordaten
+	private String editorObjects = "leveldata/editorobjects.txt";
 	
 	// Leveldatenelemente
 	private String levelPath;
@@ -37,19 +45,51 @@ public class Leveleditor extends JFrame implements ActionListener{
 	private int startX, startY;	// Startwert Spielfigur 
 	private int startLive; // Lebenspunkte zu beginn des Levels
 	
+	// Levellisten
+	private ArrayList<Door> doors = new ArrayList<Door>();
+	private ArrayList<Wall> walls = new ArrayList<Wall>();
+	private ArrayList<Ground> grounds = new ArrayList<Ground>();
+	private ArrayList<Enemy> enemys = new ArrayList<Enemy>();
+	private ArrayList<Item> items = new ArrayList<Item>();
+	private ArrayList<Checkpoint> checkpoints = new ArrayList<Checkpoint>();
+	private ArrayList<NPC> npcs = new ArrayList<NPC>();
+	private FinalEnemy finalEnemy;
+	private ArrayList<GameElement> room = new ArrayList<GameElement>();
+	
+	// Editor-Levelelemente
+	private ArrayList<Door> doorTypes = new ArrayList<Door>();
+	private ArrayList<Wall> wallTypes = new ArrayList<Wall>();
+	private ArrayList<Ground> groundTypes = new ArrayList<Ground>();
+	private ArrayList<Enemy> enemyTypes = new ArrayList<Enemy>();
+	private ArrayList<Item> itemTypes = new ArrayList<Item>();
+	private ArrayList<Checkpoint> checkpointTypes = new ArrayList<Checkpoint>();
+	private ArrayList<NPC> npcTypes = new ArrayList<NPC>();
+	private ArrayList<FinalEnemy> finalEnemyTypes = new ArrayList<FinalEnemy>();
+	
 	// Fenstergröße
 	private int windowSizeX = 1200;	
 	private int windowSizeY = 800;
 	private Color backgroundColor = Color.BLACK;
+	
+	// Spielfelddaten
+	private int fieldSizeX = 800;	
+	private int fieldSizeY = 560;
+	private int blockSize = 40;
 	
 	// Fensterelemente, Buttons, usw
 	JPanel edit = new JPanel();
 	JButton bNewMap;
 	JButton bLoadMap;
 	
+	JButton bSwitchRoom;
+	JButton bSwitchExit0;
+	JButton bSwitchExit1;
+	JButton bSwitchExit2;
+	JButton bSwitchExit3;
+	
 	// Statusvariablen
 	boolean levelLoaded;
-	int room;
+	int visibleRoom;
 	
 	// Konstruktor
 	public Leveleditor(){
@@ -65,16 +105,19 @@ public class Leveleditor extends JFrame implements ActionListener{
 		startEditor();
 		add(edit);
 		setVisible(true);
+		
+		visibleRoom = 0;
 	}
 	
 	private void startEditor(){
+		
 		edit.setLayout(null); // um Buttons beliebig zu positionieren
 		edit.setBackground(backgroundColor);
 		
+		// Menü
 		// Startbuttons hinzufügen
 		bNewMap = new JButton("Level erstellen");
 		bLoadMap = new JButton("Level bearbeiten");
-		
 		// Lege Standartpositionen für Buttons fest
 		int buttonPosX = windowSizeX/2-100;
 		int buttonPosY = windowSizeY/2-40;
@@ -88,11 +131,54 @@ public class Leveleditor extends JFrame implements ActionListener{
 		bNewMap.addActionListener(this);
 		bLoadMap.addActionListener(this);
 		// füge Buttons zum Panel hinzu
-		add(bNewMap);
-		add(bLoadMap);
+		edit.add(bNewMap);
+		edit.add(bLoadMap);
 		// Buttons sichtbar machen
 		bNewMap.setVisible(true);
 		bLoadMap.setVisible(true);
+		
+		// Editor
+		// Buttons hinzufügen
+		bSwitchRoom = new JButton("Room");
+		bSwitchExit0 = new JButton("Exit0");
+		bSwitchExit1 = new JButton("Exit1");
+		bSwitchExit2 = new JButton("Exit2");
+		bSwitchExit3 = new JButton("Exit3");
+		
+		// bestimme Position und Größe
+		bSwitchRoom.setBounds(110,fieldSizeY+50,90,30);
+		bSwitchExit0.setBounds(110,fieldSizeY+10,90,30);
+		bSwitchExit1.setBounds(210,fieldSizeY+50,90,30);
+		bSwitchExit2.setBounds(110,fieldSizeY+90,90,30);
+		bSwitchExit3.setBounds(10,fieldSizeY+50,90,30);
+		
+		// benenne Aktionen
+		bSwitchRoom.setActionCommand("switchRoom");
+		bSwitchExit0.setActionCommand("switchExit0");
+		bSwitchExit1.setActionCommand("switchExit1");
+		bSwitchExit2.setActionCommand("switchExit2");
+		bSwitchExit3.setActionCommand("switchExit3");
+		
+		// ActionListener hinzufügen
+		bSwitchRoom.addActionListener(this);
+		bSwitchExit0.addActionListener(this);
+		bSwitchExit1.addActionListener(this);
+		bSwitchExit2.addActionListener(this);
+		bSwitchExit3.addActionListener(this);
+		
+		// füge Buttons zum Panel hinzu
+		edit.add(bSwitchRoom);
+		edit.add(bSwitchExit0);
+		edit.add(bSwitchExit1);
+		edit.add(bSwitchExit2);
+		edit.add(bSwitchExit3);
+		
+		// Buttons unsichtbar machen
+		bSwitchRoom.setVisible(false);
+		bSwitchExit0.setVisible(false);
+		bSwitchExit1.setVisible(false);
+		bSwitchExit2.setVisible(false);
+		bSwitchExit3.setVisible(false);
 	}
 	
 	private void loadLevel(String path){
@@ -350,9 +436,329 @@ public class Leveleditor extends JFrame implements ActionListener{
 			}
 		}
 		if(levelLoaded){
-			// Buttons entfernen wenn Editor gestartet
-			bNewMap.setVisible(false);
-			bLoadMap.setVisible(false);
+			initWorkspace();
+		}
+	}
+	
+	private void initWorkspace(){
+		String line = null;
+		String request = "";
+		String type;
+		int typeNumber;
+		
+		// Buttons entfernen wenn Editor gestartet
+		bNewMap.setVisible(false);
+		bLoadMap.setVisible(false);
+		
+		// Arbeitsfläche erstellen
+		bSwitchRoom.setVisible(true);
+		bSwitchExit0.setVisible(true);
+		bSwitchExit1.setVisible(true);
+		bSwitchExit2.setVisible(true);
+		bSwitchExit3.setVisible(true);
+		
+		// ersten Raum initialisieren
+		initRoom(visibleRoom);
+		repaint();
+		
+		// Verfügbare Objekte laden
+		try{
+			InputStream is = getClass().getResourceAsStream(editorObjects);
+			InputStreamReader reader = new InputStreamReader(is);
+			BufferedReader in = new BufferedReader(reader);
+			
+			while ((line = in.readLine()) != null){
+				StringTokenizer tokens = new StringTokenizer(line);
+				
+				if(line.length()>0) request = tokens.nextToken();
+				else request = "";
+				
+				// je 1 Objekt jeder Art erstellen
+				while(tokens.hasMoreTokens()){
+					type = tokens.nextToken();
+					int t10 = type.charAt(0)-48;
+					int t01 = type.charAt(1)-48;
+					typeNumber = 10*t10+t01+48;
+					
+					if(request.equals("WALL")){
+						Wall w = new Wall(0,0,typeNumber);
+						wallTypes.add(w);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("GROUND")){
+						Ground g = new Ground(0,0,typeNumber);
+						groundTypes.add(g);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("DOOR")){
+						Door d = new Door(0,0,typeNumber,false);
+						doorTypes.add(d);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("CHECKPOINT")){
+						Checkpoint c = new Checkpoint(0,0,typeNumber);
+						checkpointTypes.add(c);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("ENEMY")){
+						Enemy e = new Enemy(0,0,typeNumber);
+						enemyTypes.add(e);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("FINAL")){
+						FinalEnemy fe = new FinalEnemy(0,0,typeNumber);
+						finalEnemyTypes.add(fe);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+					else if(request.equals("ITEM")){
+						Item it = new Item(0,0,typeNumber);
+						itemTypes.add(it);
+						System.out.println(request +" Type: " + (typeNumber-48) +" = " + it.getItemType() + " hinzugefügt.");
+					}
+					else if(request.equals("NPC")){
+						NPC npc = new NPC(0,0,typeNumber);
+						npcTypes.add(npc);
+						System.out.println(request +" Type: " + (typeNumber-48) + " hinzugefügt.");
+					}
+				}
+				
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Objektdatei nicht gefunden, oder fehlerhaft.");
+		}
+		
+		// Benutzeroberfläche Laden - Buttons, Textfelder, usw erstellen
+		
+		
+	}
+	
+	// Raum erstellen
+	public void initRoom(int roomnumber){
+		int x = 0;
+		int y = 0;
+			
+		Wall wall;
+		Ground ground;
+		Enemy enemy;
+		Item item;
+		Door door;
+		NPC npc;
+			
+		char element;
+		char type;
+			
+		doors.clear();
+		walls.clear();
+		npcs.clear();
+		grounds.clear();
+		enemys.clear();
+		items.clear();
+		checkpoints.clear();
+		finalEnemy = null;
+			
+		// Endgegner, falls vorhanden, auslesen
+		if(roomnumber == endBossRoom){
+			element = endBossLocation.charAt(0);
+			type = endBossLocation.charAt(1);
+			
+			// Koordinaten der Gegner aus String filtern und in Pixel umwandeln
+			int x10 = endBossLocation.charAt(6)-48;
+			int x01 = endBossLocation.charAt(7)-48;
+			int fEnemyX = (10*x10 + x01)*blockSize;
+						
+			int y10 = endBossLocation.charAt(9)-48;
+			int y01 = endBossLocation.charAt(10)-48;
+			int fEnemyY = (10*y10 + y01)*blockSize;
+			
+			finalEnemy = new FinalEnemy(fEnemyX, fEnemyY, type);
+		}
+			
+		// Raum auslesen aus leveldata
+		for(int i=0; i<leveldata[roomnumber].length(); i+=3){
+			
+			// Element und Typ(Version) aus String lesen
+			element = leveldata[roomnumber].charAt(i);
+			type = leveldata[roomnumber].charAt(i+1);
+			
+			// Beginne Hinzufügen von Elementen zu Array Lists
+			if(element == 'W'){
+				wall = new Wall(x, y, type);
+				walls.add(wall);
+			}
+			else if(element == 'G'){
+				ground = new Ground(x, y, type);
+				grounds.add(ground);
+			}			
+				
+			// erhöhe x, y um 1 Block
+			x+=blockSize;
+			// Falls Bildschirm verlassen -> neue Zeile
+			if(x>=fieldSizeX){
+				x=0;
+				y+=blockSize;
+			}
+		}
+			
+		// Türen auslesen aus doordata	
+		for(int d=0;d<doordata[roomnumber].length(); d+=11){
+			element = doordata[roomnumber].charAt(d);
+			type = doordata[roomnumber].charAt(d+1);
+				
+			// Position der Tür aus String filtern und in Pixel umwandeln
+			int x10 = doordata[roomnumber].charAt(d+3)-48;
+			int x01 = doordata[roomnumber].charAt(d+4)-48;
+			int doorX = (10*x10 + x01)*blockSize;
+						
+			int y10 = doordata[roomnumber].charAt(d+6)-48;
+			int y01 = doordata[roomnumber].charAt(d+7)-48;
+			int doorY = (10*y10 + y01)*blockSize;
+				
+			// Zustand Tür ermitteln und Tür erstellen
+			char zc = doordata[roomnumber].charAt(d+9);
+			if(zc == 'c'){
+				door = new Door(doorX, doorY, type, false);
+			}
+			else{
+				door = new Door(doorX, doorY, type, true);
+			}
+			doors.add(door);
+				
+		}
+			
+		// Gegner auslesen aus enemydata
+		for(int j=0; j<enemydata[roomnumber].length(); j+=9){
+			// Gegnertyp
+			element = enemydata[roomnumber].charAt(j);
+			type = enemydata[roomnumber].charAt(j+1);
+
+			// Koordinaten der Gegner aus String filtern und in Pixel umwandeln
+			int x10 = enemydata[roomnumber].charAt(j+3)-48;
+			int x01 = enemydata[roomnumber].charAt(j+4)-48;
+			int enemyX = (10*x10 + x01)*blockSize;
+			
+			int y10 = enemydata[roomnumber].charAt(j+6)-48;
+			int y01 = enemydata[roomnumber].charAt(j+7)-48;
+			int enemyY = (10*y10 + y01)*blockSize;
+			
+			// Gegner erstellen und in Liste einfügen
+			if(element == 'E'){
+				enemy = new Enemy(enemyX, enemyY, type);
+				enemys.add(enemy);
+			}
+		}
+			
+		// Items auslesen aus Itemdata
+		for(int k=0;k<itemdata[roomnumber].length(); k+=11){
+			// Itemtyp
+			element = itemdata[roomnumber].charAt(k);
+			type = itemdata[roomnumber].charAt(k+1);
+				
+			// Koordinaten in px auslesen
+			int x100 = itemdata[roomnumber].charAt(k+3)-48;
+			int x010 = itemdata[roomnumber].charAt(k+4)-48;
+			int x001 = itemdata[roomnumber].charAt(k+5)-48;
+			int itemX = 100*x100+10*x010+x001;
+
+			int y100 = itemdata[roomnumber].charAt(k+7)-48;
+			int y010 = itemdata[roomnumber].charAt(k+8)-48;
+			int y001 = itemdata[roomnumber].charAt(k+9)-48;
+			int itemY = 100*y100+10*y010+y001;
+				
+			// Itemtyp ermitteln (I -> 0 -> Standartitems, J ->1 -> Waffen)
+			if(element == 'J') type += 10;
+				
+			// Item erstellen und in Liste einfügen
+			item = new Item(itemX, itemY, type);
+			items.add(item);
+				
+		}		
+			
+		// Interaktive Elemente auslesen - Checkpoints, Shop, NPC
+		for(int l = 0; l<interactdata[roomnumber].length(); l+=11){
+			// Typ
+			element = interactdata[roomnumber].charAt(l);
+			type = interactdata[roomnumber].charAt(l+1);
+							
+			// Koordinaten in px auslesen
+			int x100 = interactdata[roomnumber].charAt(l+3)-48;
+			int x010 = interactdata[roomnumber].charAt(l+4)-48;
+			int x001 = interactdata[roomnumber].charAt(l+5)-48;
+			int interX = 100*x100+10*x010+x001;
+
+			int y100 = interactdata[roomnumber].charAt(l+7)-48;
+			int y010 = interactdata[roomnumber].charAt(l+8)-48;
+			int y001 = interactdata[roomnumber].charAt(l+9)-48;
+			int interY = 100*y100+10*y010+y001;
+							
+			// Element erstellen und in Liste einfügen
+			if(element == 'C'){
+				// Checkpoint
+				Checkpoint cp;
+				cp = new Checkpoint(interX, interY, type);
+				checkpoints.add(cp);
+			}
+			else if(element == 'N'){
+				// NPC 
+				npc = new NPC(interX,interY,type);
+				npcs.add(npc);
+			}
+			else if(element == 'S'){
+				// Shop 
+				type+=10;
+				npc = new NPC(interX,interY,type);
+				npcs.add(npc);
+			}
+		}
+					
+			
+	}
+		
+		// Raum zeichnen - aus Game()
+	public void buildRoom(Graphics g){
+		room.clear();
+		// Raumelemente einfügen
+		room.addAll(walls);
+		room.addAll(grounds);
+		room.addAll(doors);
+		room.addAll(checkpoints);
+		// Items einfügen - wichtig: nach Raum
+		room.addAll(items);
+		room.addAll(npcs);
+		// Gegner einfügen - wichtig: Erst Raum und Items, dann andere Objekte
+		room.addAll(enemys);
+			
+		if(finalEnemy!=null) room.add(finalEnemy);
+			
+		for(int i=0; i<room.size(); i++){
+			// Element holen und zeichnen
+			GameElement element = (GameElement)room.get(i);
+			g.drawImage(element.getImage(),element.getX(),element.getY(), this);
+		}
+	}
+		
+	public void paint(Graphics g){
+		super.paint(g);
+		if(levelLoaded){
+			// im Editor zeichnen
+			buildRoom(g);
+			
+			// Labels und Infos zeichnen
+			Font small = new Font("Arial", Font.BOLD, 14);
+			g.setColor(Color.white);
+			g.setFont(small);
+			String vRoom = ""+visibleRoom;
+			String exit0 = ""+exitdata[visibleRoom][0];
+			String exit1 = ""+exitdata[visibleRoom][1];
+			String exit2 = ""+exitdata[visibleRoom][2];
+			String exit3 = ""+exitdata[visibleRoom][3];
+			
+			g.drawString(vRoom, 145, fieldSizeY+190);
+			g.setColor(Color.red);
+			g.drawString(exit0, 145, fieldSizeY+160);
+			g.drawString(exit1, 175, fieldSizeY+190);
+			g.drawString(exit2, 145, fieldSizeY+220);
+			g.drawString(exit3, 115, fieldSizeY+190);
 		}
 	}
 	
@@ -362,6 +768,27 @@ public class Leveleditor extends JFrame implements ActionListener{
 		
 		if(levelLoaded){
 			// im Editor
+			if(action.equals("switchRoom")){
+				visibleRoom++;
+				if(visibleRoom>=rooms) visibleRoom = 0;
+				initRoom(visibleRoom);
+			}
+			else if(action.equals("switchExit0")){
+				exitdata[visibleRoom][0]++;
+				if(exitdata[visibleRoom][0]>=rooms) exitdata[visibleRoom][0] = -2;
+			}
+			else if(action.equals("switchExit1")){
+				exitdata[visibleRoom][1]++;
+				if(exitdata[visibleRoom][1]>=rooms) exitdata[visibleRoom][1] = -2;
+			}
+			else if(action.equals("switchExit2")){
+				exitdata[visibleRoom][2]++;
+				if(exitdata[visibleRoom][2]>=rooms) exitdata[visibleRoom][2] = -2;
+			}
+			else if(action.equals("switchExit3")){
+				exitdata[visibleRoom][3]++;
+				if(exitdata[visibleRoom][3]>=rooms) exitdata[visibleRoom][3] = -2;
+			}
 		}
 		else{
 			// im Editor-Menü
@@ -385,6 +812,7 @@ public class Leveleditor extends JFrame implements ActionListener{
 		        
 			}
 		}
+		repaint();
 		
 	}
 }
